a871da72fc709fd51640dad933925161b14363ed
Rami <icnocop@users.noreply.github.com>
2021-06-11 14:36:51 -0700

    Initial commit

---
+++ ClassLibrary/EnterpriseLibraryLogger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Enterprise library logger
+    /// </summary>
+    public static class EnterpriseLibraryLogger
+    {
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="properties">The properties.</param>
+        public static void LogInfo(string message, IDictionary<string, string> properties = null)
+        {
+            TraceEventType traceEventType = TraceEventType.Information;
+
+            IDictionary<string, object> parameters = properties
+                .ToDictionary(pair => pair.Key, pair => (object)pair.Value);
+
+            Logger.Write(
+                new LogEntry(
+                    message,
+                    "Information",
+                    0,
+                    100,
+                    traceEventType,
+                    "License Server",
+                    parameters));
+        }
+    }
+}
